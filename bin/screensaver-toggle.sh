6cbc7cd89a5ba219f6fa6e3f01b12a5b38413b30
Bryan Pon <bryan@bre.ad>
2013-06-10 01:46:33 -0700

    make sure the screensaver daemon is running

--- bin/screensaver-toggle.sh
+++ bin/screensaver-toggle.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Start screensaver daemon if not started
+if [ -z "`pgrep -f xscreensaver`" ]; then
+    xscreensaver -nosplash >/dev/null 2>&1 &
+fi
+
+# Toggle caffeine
 if [ -z "`pgrep -f 'caffeine --activate'`" ]; then
     caffeine --activate
 else
