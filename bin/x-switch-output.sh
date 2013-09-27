f26c4580e447d117d8c063431ad07e1f7ea8bc60
Bryan Pon <bryanpon@gmail.com>
2013-09-26 22:36:38 -0700

    reduce resolution to mirror displays

--- bin/x-switch-output.sh
+++ bin/x-switch-output.sh
@@ -13,8 +13,8 @@ display1=`xrandr --current | grep " connected " | cut -d " " -f 1 | grep HDMI | 
 
 if [ -n "$display1" ]; then
     if [ "$audio_output" == "hdmi" ]; then
-        xrandr --output "$display1" --auto --right-of "$display0"
+        xrandr --output "$display0" --mode 1920x1080 --output "$display1" --auto --same-as "$display0"
     else
-        xrandr --output "$display1" --off
+        xrandr --output "$display0" --auto --output "$display1" --off
     fi
 fi
